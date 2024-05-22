04962862d9a4dc6e442bb33a8cafbd2005b63c82
Mostafa Elfaki <mostafa@Mostafas-MacBook-Pro-688.local>
2024-05-22 15:21:02 -0700

    Staging modified, deleted, and untracked files

---
+++ Lab7Temp/src/components/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+
+</script>
+
+<main>
+
+  
+    <h1 style="font-family: 'Bebas Neue', sans-serif">Something Angry</h1>
+    <h2 style="font-family: 'Bebas Neue', sans-serif">
+
+    This is where we describe the intro
+
+
+
+    </h2>
+    <body></body>
+
+    <style>
+
+        body {
+           
+            text-align: center; /* Center text horizontally */
+            background-color: RGB(255, 255, 255);
+            
+        
+        }
+
+    </style>
+
+
+
+    
+
+    
+
+</main>
+
+<style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+  body {
+    font-family: "Bebas Neue", sans-serif;
+  }  
+
+
+
+</style>
