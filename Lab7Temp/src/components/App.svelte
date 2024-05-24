ac71b4ea9d189d9da7955ef428dc35818e45887f
Mostafa Elfaki <mostafa@Mostafas-MacBook-Pro-688.local>
2024-05-24 16:27:02 -0700

    Fixing modified and untracked files

--- Lab7Temp/src/components/App.svelte
+++ Lab7Temp/src/components/App.svelte
@@ -2,51 +2,36 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
 
+  var data = [
+      { year: "2006", alex: "104", mindy: "152", sean: "90", karen: "162" },
+      { year: "2007", alex: "122", mindy: "184", sean: "99", karen: "143" },
+      { year: "2008", alex: "50", mindy: "201", sean: "127", karen: "114" },
+      { year: "2009", alex: "150", mindy: "134", sean: "139", karen: "80" }
+  ];
+
+  var colors = ["#C9D6DF", "#F7EECF", "#E3E1B2", "#F9CAC8"];
+
+  var stack = d3.stack().keys(["alex", "mindy", "sean", "karen"]);
+  var dataset = stack(data.map(d => ({
+    x: d3.timeParse("%Y")(d.year),
+    y: [+d.alex, +d.mindy, +d.sean, +d.karen]
+  })));
+
+  
+
 
 
 </script>
 
 <main>
-
-  
-    <h1 style="font-family: 'Bebas Neue', sans-serif">Something Angry</h1>
-    <p style="font-family: 'Bebas Neue', sans-serif">
-
-    This is where we describe the intro
-
-
-
-    </p>
-    <body></body>
-
-    <style>
-
-        body {
-           
-            text-align: center; /* Center text horizontally */
-            background-color: RGB(255, 255, 255);
-            
-        
-        }
-
-    </style>
-
-
-
-    
-
-    
-
+  <h1>Hello</h1>
+  <body></body>
 </main>
 
 <style>
-
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
   body {
     font-family: "Bebas Neue", sans-serif;
-  }  
-
-
-
+  }
 </style>
